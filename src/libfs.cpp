67d98e9a4ec2860fa1239fa602a85821cf444bb6
Jack.Win <foxintango@yeah.net>
2023-11-18 15:28:07 +0800

    alpine

---
+++ src/libfs.cpp
@@ -0,0 +1,3 @@
+#include "libfs.h"
+
+void libfs_init(){}
---
+++ src/libfs.h
@@ -0,0 +1,11 @@
+#ifndef _LIB_FS_H_
+#define _LIB_FS_H_
+#include <libcpp/libcpp.h>
+EXTERN_C_BEGIN
+/**
+ * 文件节点[文件,链接,文件夹],创建,删除,读取,写入,命名
+ */
+
+void foxintangoAPI libfs_init();
+EXTERN_C_END
+#endif
